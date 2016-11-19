3ec4eed3c41757f339b0086222274b3ae271bc02
TahaKhz <taha.khazaee@gmail.com>
2016-11-19 11:14:12 +0330

    just remember with a table that contains ports name you can manapulate the URI of the website

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+
+
+include_once('simple_html_dom.php');
+
+
+
+
+
+            $target_url ='http://www.marinetraffic.com/en/ais/details/ports/'.$portname.'';
+            $html = new simple_html_dom();
+            $html->load_file($target_url);
+            $crawlds[++$counts] = array();
+ 
+            foreach ($html->find('div[class=bg-info] b a') as $post) {
+                array_push($craws[$counts], strip_tags($post));
+			}}
+
+
+echo '<pre>';
+var_dump($crawls);
+echo '</pre>';
+
+
+
+
+?>
+
+
+
+
+
